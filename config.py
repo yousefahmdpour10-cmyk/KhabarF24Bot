351685b02df343d157a095a30334a61f793ea3ea
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 02:35:19 +0330

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-# اطلاعات ربات
-BOT_TOKEN = ""
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = "@KhabarF24"
 
-# زمان بررسی اخبار (ثانیه)
 CHECK_INTERVAL = 300
 
-# دسته‌بندی‌ها
 ENABLE_WORLD = True
 ENABLE_IRAN = True
 ENABLE_POLITICS = True
@@ -13,5 +12,4 @@ ENABLE_SPORT = True
 ENABLE_ECONOMY = True
 ENABLE_TECH = True
 
-# محدودیت انتشار
 MAX_POSTS_PER_HOUR = 30
