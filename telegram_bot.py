b7a5802d7915bfa554d95141fbbe3d0e5d2d7611
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 02:38:27 +0330

    telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,10 @@
+from telegram import Bot
+from config import BOT_TOKEN, CHANNEL_ID
 
+bot = Bot(token=BOT_TOKEN)
+
+async def send_message(text):
+    await bot.send_message(
+        chat_id=CHANNEL_ID,
+        text=text
+    )
