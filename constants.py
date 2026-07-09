9a4b880a70a1eea01b22e82a039f552e82bfcb70
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 02:51:52 +0330

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,17 @@
+# Project Information
+PROJECT_NAME = "KhabarF24 Engine"
+VERSION = "0.1.0"
+
+# Channel
+CHANNEL_USERNAME = "@KhabarF24"
+
+# Categories
+BREAKING = "🔴 خبر فوری"
+WORLD = "🌍 جهان"
+IRAN = "🇮🇷 ایران"
+SPORT = "⚽ ورزش"
+ECONOMY = "💰 اقتصاد"
+TECH = "💻 فناوری"
+
+# Footer
+FOOTER = "\n━━━━━━━━━━━━\n📢 @KhabarF24"
