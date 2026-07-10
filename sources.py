8549ce38bc3f8ebf6097cd388ada84bd85d8acf2
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 11:39:33 +0330

    Create sources.py

---
+++ sources.py
@@ -0,0 +1,14 @@
+RSS_SOURCES = {
+    "world": [
+        "https://feeds.reuters.com/reuters/worldNews",
+        "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+    ],
+
+    "technology": [
+        "https://feeds.arstechnica.com/arstechnica/index"
+    ],
+
+    "sport": [
+        "https://www.espn.com/espn/rss/news"
+    ]
+}
