886bacb58aa26566d794d0e1cd8592b7890a2b09
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 16:21:52 +0330

    Create news_db.py

---
+++ news_db.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+DB_NAME = "news.db"
+
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS published_news (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        link TEXT UNIQUE
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def is_published(link):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT 1 FROM published_news WHERE link=?",
+        (link,)
+    )
+
+    result = cursor.fetchone()
+
+    conn.close()
+
+    return result is not None
+
+
+def mark_as_published(link):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO published_news(link) VALUES(?)",
+        (link,)
+    )
+
+    conn.commit()
+    conn.close()
