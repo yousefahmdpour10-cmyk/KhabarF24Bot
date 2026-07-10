e114fc7298587c1e89229d8d8e2df695631d8859
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 03:41:18 +0330

    telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,7 +4,12 @@ from config import BOT_TOKEN, CHANNEL_ID
 bot = Bot(token=BOT_TOKEN)
 
 async def send_message(text):
-    await bot.send_message(
-        chat_id=CHANNEL_ID,
-        text=text
-    )
+    try:
+        await bot.send_message(
+            chat_id=CHANNEL_ID,
+            text=text
+        )
+        print("✅ Message sent successfully.")
+
+    except Exception as e:
+        print(f"❌ Telegram Error: {e}")
