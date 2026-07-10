822f31ba0dc493f8d7f14fbf67133a5cc6265a74
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 14:59:02 +0330

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import asyncio
+
+from news_fetcher import get_latest_news
 from telegram_bot import send_message
 
+
 async def main():
-    await send_message("🤖 KhabarF24Bot Online")
+    news = get_latest_news()
+
+    if not news:
+        print("No news found.")
+        return
+
+    first_news = news[0]
+
+    message = f"""📰 {first_news['title']}
+
+🔗 {first_news['link']}
+"""
+
+    await send_message(message)
+
 
 asyncio.run(main())
