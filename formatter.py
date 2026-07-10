1eb465aa5ec1719085c816c6d02b71d78fe7b722
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-10 21:11:03 +0330

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,1 +1,19 @@
+def format_news(news):
 
+    category = news.get("category", "🌍 جهان")
+    source = news.get("source", "منبع نامشخص")
+
+    message = f"""{category}
+
+📰 {news["title"]}
+
+🗞 {source}
+
+📢 @KhabarF24
+
+━━━━━━━━━━━━
+
+#خبر
+"""
+
+    return message
