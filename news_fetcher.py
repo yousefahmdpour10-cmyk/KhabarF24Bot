afa0fe558f957350d6cacea8b30dd654b2f45652
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-11 18:36:43 +0330

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,13 +1,19 @@
 from rss_fetcher import fetch_news
 from sources import RSS_SOURCES
 
+
 def get_latest_news():
+
     news = []
 
-    for url in RSS_SOURCES["world"]:
-        try:
-            news.extend(fetch_news(url))
-        except Exception as e:
-            print(f"RSS Error: {e}")
+    for category in RSS_SOURCES.values():
+
+        for url in category:
+
+            try:
+                news.extend(fetch_news(url))
+
+            except Exception as e:
+                print(f"RSS Error: {e}")
 
     return news
