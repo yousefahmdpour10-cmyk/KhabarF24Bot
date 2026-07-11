53bb9e8c619ca93ffc498a63e6ce68a74054a81a
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-11 17:51:27 +0330

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,14 +1,45 @@
 RSS_SOURCES = {
+
     "world": [
-        "https://feeds.reuters.com/reuters/worldNews",
-        "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+
+        # World News
+        "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+        "https://feeds.bbci.co.uk/news/world/rss.xml",
+        "https://www.aljazeera.com/xml/rss/all.xml",
+
+    ],
+
+    "iran": [
+
+        # فعلاً خالی
     ],
 
     "technology": [
-        "https://feeds.arstechnica.com/arstechnica/index"
+
+        "https://feeds.arstechnica.com/arstechnica/index",
+        "https://www.theverge.com/rss/index.xml",
+        "https://techcrunch.com/feed/",
+
     ],
 
     "sport": [
-        "https://www.espn.com/espn/rss/news"
+
+        "https://www.espn.com/espn/rss/news",
+
+    ],
+
+    "gaming": [
+
+        "https://news.ubisoft.com/rss",
+        "https://blog.playstation.com/feed/",
+        "https://news.xbox.com/en-us/feed/",
+
+    ],
+
+    "economy": [
+
+        # فعلاً خالی
+
     ]
+
 }
