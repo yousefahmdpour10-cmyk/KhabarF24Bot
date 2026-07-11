9bd7a687ab67fb98d4da190f05835418a1aef157
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-11 21:29:56 +0330

    Update rss_fetcher.py

--- rss_fetcher.py
+++ rss_fetcher.py
@@ -2,6 +2,7 @@ import feedparser
 
 
 def fetch_news(rss_url):
+
     feed = feedparser.parse(rss_url)
 
     if feed.bozo:
@@ -10,10 +11,25 @@ def fetch_news(rss_url):
     news = []
 
     for entry in feed.entries:
+
+        source = ""
+
+        if "source" in entry:
+            source = entry.source.get("title", "").strip()
+
+        if not source:
+            source = feed.feed.get("title", "").strip()
+
         news.append({
+
             "title": entry.get("title", "").strip(),
+
             "link": entry.get("link", "").strip(),
-            "summary": entry.get("summary", "").strip()
+
+            "summary": entry.get("summary", "").strip(),
+
+            "source": source
+
         })
 
     return news
