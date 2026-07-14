2f0f8dddf350cd1b69a2a9553fa5e633ce4cde54
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-14 23:48:03 +0330

    Update brand_dictionary.py

--- brand_dictionary.py
+++ brand_dictionary.py
@@ -1,102 +1,84 @@
 """
 brand_dictionary.py
 
-KhabarF24 Official Names Dictionary
+KhabarF24 Official Names Dictionary v4.1
 
-حفظ نام رسمی:
-- باشگاه‌ها
-- تیم‌ها
-- بازیکنان
-- لیگ‌ها
-- برندها
+نام رسمی فارسی + نام اصلی
 """
-
 
 BRAND_DICTIONARY = {
 
 
-    # =========================
     # ⚽ Football Clubs
-    # =========================
 
-    "Manchester United": "منچستر یونایتد",
-    "Manchester City": "منچستر سیتی",
-    "Real Madrid": "رئال مادرید",
-    "Barcelona": "بارسلونا",
-    "FC Barcelona": "بارسلونا",
-    "Liverpool": "لیورپول",
-    "Arsenal": "آرسنال",
-    "Chelsea": "چلسی",
-    "Bayern Munich": "بایرن مونیخ",
-    "Paris Saint-Germain": "پاری‌سن‌ژرمن",
-    "PSG": "پاری‌سن‌ژرمن",
-    "Inter Milan": "اینتر میلان",
-    "AC Milan": "آث میلان",
-    "Juventus": "یوونتوس",
-    "Borussia Dortmund": "بوروسیا دورتموند",
-    "Atletico Madrid": "اتلتیکو مادرید",
-
-
-    # =========================
-    # ⚽ Players
-    # =========================
-
-    "Lionel Messi": "لیونل مسی",
-    "Cristiano Ronaldo": "کریستیانو رونالدو",
-    "Kylian Mbappe": "کیلیان امباپه",
-    "Jude Bellingham": "جود بلینگهام",
-    "Lamine Yamal": "لامین یامال",
-    "Erling Haaland": "ارلینگ هالند",
-    "Mohamed Salah": "محمد صلاح",
-    "Vinicius Junior": "وینیسیوس جونیور",
+    "Manchester United": "منچستر یونایتد (Manchester United)",
+    "Manchester City": "منچستر سیتی (Manchester City)",
+    "Real Madrid": "رئال مادرید (Real Madrid)",
+    "Barcelona": "بارسلونا (Barcelona)",
+    "FC Barcelona": "بارسلونا (Barcelona)",
+    "Liverpool": "لیورپول (Liverpool)",
+    "Arsenal": "آرسنال (Arsenal)",
+    "Chelsea": "چلسی (Chelsea)",
+    "Bayern Munich": "بایرن مونیخ (Bayern Munich)",
+    "Paris Saint-Germain": "پاری‌سن‌ژرمن (Paris Saint-Germain)",
+    "PSG": "پاری‌سن‌ژرمن (PSG)",
+    "Inter Milan": "اینتر میلان (Inter Milan)",
+    "AC Milan": "آث میلان (AC Milan)",
+    "Juventus": "یوونتوس (Juventus)",
+    "Borussia Dortmund": "بوروسیا دورتموند (Borussia Dortmund)",
+    "Atletico Madrid": "اتلتیکو مادرید (Atletico Madrid)",
 
 
-    # =========================
+    # 👤 Players
+
+    "Lionel Messi": "لیونل مسی (Lionel Messi)",
+    "Cristiano Ronaldo": "کریستیانو رونالدو (Cristiano Ronaldo)",
+    "Kylian Mbappe": "کیلیان امباپه (Kylian Mbappe)",
+    "Jude Bellingham": "جود بلینگهام (Jude Bellingham)",
+    "Lamine Yamal": "لامین یامال (Lamine Yamal)",
+    "Erling Haaland": "ارلینگ هالند (Erling Haaland)",
+    "Mohamed Salah": "محمد صلاح (Mohamed Salah)",
+    "Vinicius Junior": "وینیسیوس جونیور (Vinicius Junior)",
+
+
     # 🏀 Basketball
-    # =========================
 
     "NBA": "NBA",
     "WNBA": "WNBA",
-    "Los Angeles Lakers": "لس‌آنجلس لیکرز",
-    "Boston Celtics": "بوستون سلتیکس",
-    "Minnesota Lynx": "مینه‌سوتا لینکس",
-    "LeBron James": "لبران جیمز",
+    "Los Angeles Lakers": "لس‌آنجلس لیکرز (Los Angeles Lakers)",
+    "Boston Celtics": "بوستون سلتیکس (Boston Celtics)",
+    "Minnesota Lynx": "مینه‌سوتا لینکس (Minnesota Lynx)",
+    "LeBron James": "لبران جیمز (LeBron James)",
 
 
-    # =========================
-    # 🏐 Other Sports
-    # =========================
+    # 🏆 Organizations
 
-    "FIFA": "فیفا",
-    "UEFA": "یوفا",
-    "AFC": "کنفدراسیون فوتبال آسیا",
-    "FIVB": "فدراسیون جهانی والیبال",
-    "United World Wrestling": "اتحادیه جهانی کشتی",
+    "FIFA": "فیفا (FIFA)",
+    "UEFA": "یوفا (UEFA)",
+    "AFC": "کنفدراسیون فوتبال آسیا (AFC)",
+    "FIVB": "فدراسیون جهانی والیبال (FIVB)",
+    "United World Wrestling": "اتحادیه جهانی کشتی (UWW)",
     "ATP": "ATP",
     "WTA": "WTA",
-    "Formula 1": "فرمول یک",
+    "Formula 1": "فرمول یک (Formula 1)",
 
 
-    # =========================
     # 💻 Technology
-    # =========================
 
-    "Apple": "اپل",
-    "Google": "گوگل",
-    "Microsoft": "مایکروسافت",
-    "OpenAI": "OpenAI",
-    "Tesla": "تسلا",
-    "NVIDIA": "انویدیا",
+    "Apple": "اپل (Apple)",
+    "Google": "گوگل (Google)",
+    "Microsoft": "مایکروسافت (Microsoft)",
+    "OpenAI": "اوپن‌ای‌آی (OpenAI)",
+    "Tesla": "تسلا (Tesla)",
+    "NVIDIA": "انویدیا (NVIDIA)",
 
 
-    # =========================
     # 🎮 Gaming
-    # =========================
 
-    "Assassin's Creed": "اساسینز کرید",
-    "PlayStation": "پلی‌استیشن",
-    "Xbox": "ایکس‌باکس",
-    "Steam": "استیم",
+    "Assassin's Creed": "اساسینز کرید (Assassin's Creed)",
+    "PlayStation": "پلی‌استیشن (PlayStation)",
+    "Xbox": "ایکس‌باکس (Xbox)",
+    "Steam": "استیم (Steam)",
 
 }
 
