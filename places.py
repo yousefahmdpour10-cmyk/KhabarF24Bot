4b20b1146b082c6eded4e5f4ea1b630ae1ee1018
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-15 00:22:20 +0330

    Create places.py

---
+++ places.py
@@ -0,0 +1,55 @@
+"""
+KhabarF24 Protected Places v4.4
+
+نام مکان‌ها و مناطق که نباید ترجمه ماشینی شوند
+"""
+
+
+PROTECTED_PLACES = [
+
+    # 🇮🇷 Iran
+
+    "Kuh-e Kolang",
+    "Kuh Kolang",
+    "Koh-e Kolang",
+
+    "Strait of Hormuz",
+    "Persian Gulf",
+
+    "Tehran",
+    "Iran",
+    "Iraq",
+    "Syria",
+
+    
+    # 🌍 Middle East
+
+    "Gaza",
+    "West Bank",
+    "Red Sea",
+
+    "Yemen",
+
+    "Lebanon",
+
+    "Israel",
+
+    "Suez Canal",
+
+
+    # 🌎 Global
+
+    "Washington",
+    "New York",
+
+    "Moscow",
+
+    "Beijing",
+
+    "Taiwan",
+
+    "Ukraine",
+
+    "Russia",
+
+]
