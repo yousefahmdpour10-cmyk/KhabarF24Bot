947458995fb0e12d6be5ea062c5fe8dac3d5a845
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-17 00:43:47 +0330

    Update game_rules.py

--- game_rules.py
+++ game_rules.py
@@ -1,217 +1,63 @@
 """
 KhabarF24 Game Rules v1.0
 
-وظیفه:
-- تشخیص نوع خبرهای گیم
-- تشخیص زیر دسته بازی
-- آماده سازی برای Formatter
-
-Categories:
-🎮 گیم
+تشخیص خبرهای گیمینگ
 """
 
-
-
-print("🎮 KhabarF24 Game Rules v1.0 Loaded")
-
-
-
-
-
-GAME_TYPES = {
-
-
-    "console": {
-
-        "emoji": "🎮",
-
-        "type": "کنسول",
-
-        "hashtag": "#گیم"
-
-    },
-
-
-    "game_release": {
-
-        "emoji": "🕹️",
-
-        "type": "معرفی بازی",
-
-        "hashtag": "#گیم"
-
-    },
-
-
-    "online_game": {
-
-        "emoji": "🌐",
-
-        "type": "بازی آنلاین",
-
-        "hashtag": "#گیم"
-
-    },
-
-
-    "studio": {
-
-        "emoji": "🏢",
-
-        "type": "استودیو بازی",
-
-        "hashtag": "#گیم"
-
-    },
-
-
-    "esport": {
-
-        "emoji": "🏆",
-
-        "type": "ورزش الکترونیک",
-
-        "hashtag": "#گیم"
-
-    }
-
-
-}
-
-
-
-
-
-GAME_KEYWORDS = {
-
-
-    "console": [
-
-        "playstation",
-
-        "ps5",
-
-        "ps4",
-
-        "xbox",
-
-        "nintendo",
-
-        "کنسول",
-
-        "پلی استیشن",
-
-        "ایکس باکس",
-
-        "نینتندو",
-
-    ],
-
-
-
-    "game_release": [
-
-        "new game",
-
-        "game release",
-
-        "release date",
-
-        "trailer",
-
-        "gameplay",
-
-        "معرفی بازی",
-
-        "عرضه بازی",
-
-        "تاریخ انتشار",
-
-        "تریلر",
-
-        "گیم پلی",
-
-    ],
-
-
-
-
-    "online_game": [
-
-        "online game",
-
-        "server",
-
-        "update",
-
-        "season",
-
-        "آپدیت",
-
-        "سرور",
-
-        "فصل جدید",
-
-        "بازی آنلاین",
-
-    ],
-
-
-
-
-    "studio": [
-
-        "ubisoft",
-
-        "electronic arts",
-
-        "ea games",
-
-        "rockstar",
-
-        "valve",
-
-        "استودیو",
-
-        "شرکت بازی سازی",
-
-        "شرکت بازی‌سازی",
-
-    ],
-
-
-
-
-    "esport": [
-
-        "esports",
-
-        "e sports",
-
-        "مسابقات بازی",
-
-        "ورزش الکترونیک",
-
-        "قهرمانی بازی",
-
-    ]
-
-}
-
-
-
-
-
-
-
-def contains_any(text, words):
-
-
-    text = text.lower()
-
-
-    for word in words:
-
+GAME_KEYWORDS = [
+
+    # کنسول
+    "playstation",
+    "ps5",
+    "ps4",
+    "xbox",
+    "nintendo",
+    "switch",
+
+    # شرکت‌ها
+    "sony",
+    "سونی",
+    "microsoft",
+    "مایکروسافت",
+    "ubisoft",
+    "یوبی‌سافت",
+    "electronic arts",
+    "ea sports",
+    "valve",
+    "steam",
+
+    # بازی‌ها
+    "call of duty",
+    "warzone",
+    "minecraft",
+    "fortnite",
+    "gta",
+    "grand theft auto",
+    "elder scrolls",
+    "fifa",
+    "fc 25",
+    "fc 26",
+
+    # فارسی
+    "گیم",
+    "بازی ویدیویی",
+    "بازی رایانه‌ای",
+    "کنسول بازی",
+    "گیمر",
+    "استودیو بازی",
+    "سازنده بازی",
+    "آپدیت بازی",
+    "نسخه جدید بازی",
+
+]
+
+
+def detect_game(title, summary=""):
+
+    text = f"{title} {summary}".lower()
+
+
+    for word in GAME_KEYWORDS:
 
         if word.lower() in text:
 
@@ -219,92 +65,3 @@ def contains_any(text, words):
 
 
     return False
-
-
-
-
-
-
-
-def detect_game_type(title="", summary=""):
-
-
-    text = f"{title} {summary}"
-
-
-
-    for game_type, keywords in GAME_KEYWORDS.items():
-
-
-        if contains_any(text, keywords):
-
-
-            return GAME_TYPES[game_type]
-
-
-
-    return {
-
-
-        "emoji": "🎮",
-
-        "type": "گیم",
-
-        "hashtag": "#گیم"
-
-    }
-
-
-
-
-
-
-
-def calculate_game_score(title="", summary=""):
-
-
-    text = f"{title} {summary}".lower()
-
-
-    score = 0
-
-
-
-    important = [
-
-        "هک",
-
-        "تعطیلی سرور",
-
-        "خرید شرکت",
-
-        "عرضه رسمی",
-
-        "نسخه جدید",
-
-        "کنسول جدید",
-
-        "جایزه",
-
-        "آپدیت بزرگ",
-
-    ]
-
-
-
-    for word in important:
-
-
-        if word in text:
-
-            score += 2
-
-
-
-    if score > 10:
-
-        score = 10
-
-
-
-    return score
