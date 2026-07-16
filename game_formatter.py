26f32e814a1bd2ea4901f1413924acb84930f56e
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-17 00:35:38 +0330

    Create game_formatter.py

---
+++ game_formatter.py
@@ -0,0 +1,20 @@
+"""
+KhabarF24 Game Formatter v1.0
+"""
+
+
+def format_game(title, summary):
+
+    return {
+
+        "title": title,
+
+        "summary": summary,
+
+        "emoji": "🎮",
+
+        "category": "gaming",
+
+        "hashtag": "#گیم"
+
+    }
