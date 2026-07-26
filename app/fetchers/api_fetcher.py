bde1e95b126798baaa4a70ea08917d26b05bae42
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-26 14:51:01 +0330

    Update api_fetcher.py

--- app/fetchers/api_fetcher.py
+++ app/fetchers/api_fetcher.py
@@ -1,1 +1,32 @@
+"""
+دریافت اخبار از API
+"""
 
+from typing import List
+
+import requests
+
+from app.fetchers.base_fetcher import BaseFetcher
+from app.models.news import News
+
+
+class APIFetcher(BaseFetcher):
+    """
+    دریافت اخبار از API
+    """
+
+    async def fetch(self) -> List[News]:
+
+        response = requests.get(
+            self.source.url,
+            timeout=20,
+        )
+
+        data = response.json()
+
+        news_list = []
+
+        # هر API ساختار مخصوص خودش را دارد.
+        # بعداً برای هر سرویس Parser جداگانه خواهیم نوشت.
+
+        return news_list
