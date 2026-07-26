57f3190d8786fcec3d12f7541abf2502c62359cc
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-26 14:49:57 +0330

    Update website_fetcher.py

--- app/fetchers/website_fetcher.py
+++ app/fetchers/website_fetcher.py
@@ -1,1 +1,36 @@
+"""
+دریافت اخبار از وب‌سایت‌هایی که RSS ندارند
+"""
 
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+from app.fetchers.base_fetcher import BaseFetcher
+from app.models.news import News
+
+
+class WebsiteFetcher(BaseFetcher):
+    """
+    دریافت اخبار با Web Scraping
+    """
+
+    async def fetch(self) -> List[News]:
+
+        response = requests.get(
+            self.source.url,
+            timeout=20,
+            headers={
+                "User-Agent": "Mozilla/5.0"
+            }
+        )
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        news_list = []
+
+        # هر سایت بعداً Parser مخصوص خودش را خواهد داشت.
+        # فعلاً فقط اسکلت کلاس را می‌سازیم.
+
+        return news_list
