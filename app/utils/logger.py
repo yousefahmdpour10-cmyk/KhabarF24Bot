ccfcfcdecc0be55752bf22db1e1f722723b9c889
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-27 22:47:34 +0330

    Update logger.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,1 +1,23 @@
+"""
+سیستم لاگ پروژه KhabarF24
+"""
 
+import logging
+import os
+
+LOG_DIR = "logs"
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = os.path.join(LOG_DIR, "khabarf24.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE, encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("KhabarF24")
