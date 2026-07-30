cdfdf5e2375a3f96a2120f1fdc0a9b0d42ba6237
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-30 20:55:34 +0330

    Create source_scores.py

---
+++ app/processors/importance/source_scores.py
@@ -0,0 +1,30 @@
+"""
+Source Importance
+"""
+
+SOURCE_SCORES = {
+
+    "Reuters": 10,
+
+    "AP": 10,
+
+    "AFP": 10,
+
+    "BBC": 9,
+
+    "CNN": 8,
+
+    "Al Jazeera": 8,
+
+    "Tasnim": 7,
+
+    "IRNA": 7,
+
+    "ISNA": 7,
+
+    "Mehr": 7,
+
+    "Fars": 7,
+
+    "KhabarFoory": 6,
+}
