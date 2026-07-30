3217a80ff67b447c259aa232a53131d613953e67
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-30 20:55:03 +0330

    Create category_scores.py

---
+++ app/processors/importance/category_scores.py
@@ -0,0 +1,22 @@
+"""
+Category Importance
+"""
+
+CATEGORY_SCORES = {
+
+    "war": 10,
+
+    "politics": 7,
+
+    "economy": 5,
+
+    "technology": 3,
+
+    "health": 6,
+
+    "weather": 7,
+
+    "sports": 2,
+
+    "general": 1,
+}
