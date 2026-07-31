3ee2a0d38cccebc22b444f56797523991874b3fe
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-31 20:10:38 +0330

    Create __init__.py

---
+++ app/formatter/templates/__init__.py
@@ -0,0 +1,10 @@
+"""
+Templates
+"""
+
+from .world import WorldTemplate
+from .iran import IranTemplate
+from .economy import EconomyTemplate
+from .technology import TechnologyTemplate
+from .health import HealthTemplate
+from .sport import SportTemplate
