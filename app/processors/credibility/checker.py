a9092e1d065ed1a3923e6576e1634c042a0cf63c
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-07-31 14:27:34 +0330

    Update checker.py

--- app/processors/credibility/checker.py
+++ app/processors/credibility/checker.py
@@ -1,1 +1,34 @@
+"""
+Credibility Checker
+"""
 
+from app.models.raw_news import RawNews
+from app.utils.logger import logger
+
+from .rules import MIN_CREDIBILITY_SCORE
+from .source_reputation import SOURCE_REPUTATION
+
+
+class CredibilityChecker:
+
+    async def process(
+        self,
+        news: RawNews,
+    ) -> RawNews:
+
+        score = SOURCE_REPUTATION.get(
+            news.source_name,
+            50,
+        )
+
+        news.credibility_score = score
+
+        news.is_verified = (
+            score >= MIN_CREDIBILITY_SCORE
+        )
+
+        logger.info(
+            f"Credibility: {score}"
+        )
+
+        return news
