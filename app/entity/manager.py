99abb93b5a3fefc2af71e15e233b79c14d4d8844
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-08-01 20:46:05 +0330

    Update manager.py

--- app/entity/manager.py
+++ app/entity/manager.py
@@ -1,31 +1,43 @@
 """
-Entity Manager
+KhabarF24 Entity Manager
 """
 
 from app.models.raw_news import RawNews
 
-from .football import FootballEntityExtractor
+from .teams import TeamEntityExtractor
+from .people import PeopleEntityExtractor
+from .leagues import LeagueEntityExtractor
+from .tournaments import TournamentEntityExtractor
+from .stadiums import StadiumEntityExtractor
+from .referees import RefereeEntityExtractor
 
 
 class EntityManager:
 
     def __init__(self):
 
-        self.football = FootballEntityExtractor()
+        self.teams = TeamEntityExtractor()
+        self.people = PeopleEntityExtractor()
+        self.leagues = LeagueEntityExtractor()
+        self.tournaments = TournamentEntityExtractor()
+        self.stadiums = StadiumEntityExtractor()
+        self.referees = RefereeEntityExtractor()
 
     def extract(
         self,
         news: RawNews,
     ) -> RawNews:
 
-        sport = getattr(
-            news,
-            "sport",
-            "",
-        ).lower()
+        news = self.teams.extract(news)
 
-        if sport == "football":
+        news = self.people.extract(news)
 
-            news = self.football.extract(news)
+        news = self.leagues.extract(news)
+
+        news = self.tournaments.extract(news)
+
+        news = self.stadiums.extract(news)
+
+        news = self.referees.extract(news)
 
         return news
