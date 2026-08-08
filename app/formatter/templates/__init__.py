8310377032be4efd71a318670ddc67543886ced4
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-08-08 15:40:11 +0330

    Update __init__.py

--- app/formatter/templates/__init__.py
+++ app/formatter/templates/__init__.py
@@ -4,6 +4,7 @@ Templates
 
 from .world import WorldTemplate
 from .iran import IranTemplate
+from .politics import PoliticsTemplate
 from .economy import EconomyTemplate
 from .technology import TechnologyTemplate
 from .health import HealthTemplate
