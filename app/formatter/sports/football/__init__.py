4354f1aacf34c2b0383ed6d6ab734530a50adc01
yousefahmdpour10-cmyk <yousef.ahmdpour10@gmail.com>
2026-09-11 16:50:44 +0330

    Update __init__.py

--- app/formatter/sports/football/__init__.py
+++ app/formatter/sports/football/__init__.py
@@ -3,15 +3,15 @@ Football Formatter
 """
 
 from app.models.raw_news import RawNews
+from app.formatter.header import build_header
 from app.formatter.footer import build_footer
 from app.formatter.hashtags import HashtagBuilder
 from app.formatter.source_flags import get_flag
+from .builder import FootballBuilder
 from app.formatter.icons import (
     TITLE,
     SOURCE,
 )
-
-from .builder import FootballBuilder
 
 
 class FootballFormatter:
@@ -29,10 +29,8 @@ class FootballFormatter:
         hashtags = self.hashtags.build(news)
         flag = get_flag(news.source)
 
-        text += "━━━━━━━━━━━━━━━━\n"
-        text += "🔴 KhabarF24 | ⚽ فوتبال\n"
-        text += "━━━━━━━━━━━━━━━━\n\n"
-
+        text += build_header("⚽ فوتبال", getattr(news, "is_breaking", False))
+        text += "\n\n"
         text += f"{TITLE} {news.title}\n\n"
 
         details = self.build_details(news)
